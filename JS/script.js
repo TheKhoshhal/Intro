3112d60b680120de1bdb7cec69d3246979b21f61
TheKhoshhal <thekhoshhal@gmail.com>
2023-10-04 00:09:43 +0330

    first commit

---
+++ JS/script.js
@@ -0,0 +1,19 @@
+"use strict";
+
+// const word = document.querySelector('h1');
+// let cond = true;
+
+// window.addEventListener('keypress', (e) => {
+// 	let KeyPress = e.key;
+// 	if (cond == true) {
+// 		word.innerText = KeyPress;
+// 	} else {
+// 		word.innerText += KeyPress;
+// 	}
+// 	cond = false;
+// });
+
+let x =
+  "wwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwww";
+
+console.log(x.length);
